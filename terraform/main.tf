1c893de6ccfea75e0eb0e477558d190f62748e76
mujju <mohdmujahed1716@gmail.com>
2026-02-12 13:24:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,9 @@
-
 provider "aws" {
   region = "eu-north-1"
 }
 
 ################################
-# Get AZ
+# Availability Zones
 ################################
 data "aws_availability_zones" "available" {}
 
@@ -13,6 +12,9 @@ data "aws_availability_zones" "available" {}
 ################################
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 ################################
@@ -40,16 +42,7 @@ resource "aws_subnet" "public_2" {
 }
 
 ################################
-# Private Subnet
-################################
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = data.aws_availability_zones.available.names[0]
-}
-
-################################
-# Public Route Table
+# Route Table (Public)
 ################################
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -60,9 +53,6 @@ resource "aws_route_table" "public" {
   }
 }
 
-################################
-# Route Table Assoc
-################################
 resource "aws_route_table_association" "pub1" {
   subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public.id
@@ -74,38 +64,10 @@ resource "aws_route_table_association" "pub2" {
 }
 
 ################################
-# NAT Gateway
-################################
-resource "aws_eip" "nat" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_1.id
-}
-
-################################
-# Private Route Table
-################################
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-}
-
-resource "aws_route_table_association" "priv" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
-
-################################
 # Security Groups
 ################################
 resource "aws_security_group" "alb_sg" {
+  name   = "alb-sg"
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -124,6 +86,7 @@ resource "aws_security_group" "alb_sg" {
 }
 
 resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -167,6 +130,10 @@ resource "aws_lb_target_group" "tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path = "/"
+  }
 }
 
 resource "aws_lb_listener" "listener" {
@@ -181,7 +148,7 @@ resource "aws_lb_listener" "listener" {
 }
 
 ################################
-# AMI
+# AMI (Amazon Linux 2023 x86)
 ################################
 data "aws_ami" "linux" {
   most_recent = true
@@ -189,19 +156,21 @@ data "aws_ami" "linux" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*"]
+    values = ["al2023-ami-*-x86_64"]
   }
 }
 
 ################################
-# EC2
+# EC2 Instance (PUBLIC)
 ################################
 resource "aws_instance" "app" {
 
   ami           = data.aws_ami.linux.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
-  subnet_id = aws_subnet.private.id
+  subnet_id = aws_subnet.public_1.id
+
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
@@ -209,20 +178,32 @@ resource "aws_instance" "app" {
 
   user_data = <<EOF
 #!/bin/bash
-dnf install -y docker awscli
+exec > /var/log/user-data.log 2>&1
+
+# Install Docker (AL2023 + AL2 safe)
+dnf install -y docker || yum install -y docker
+
+systemctl enable docker
 systemctl start docker
 
-aws ecr get-login-password --region eu-north-1 | \
-docker login --username AWS --password-stdin ${aws_ecr_repository.app.repository_url}
+# Wait for network
+sleep 20
 
-docker pull ${aws_ecr_repository.app.repository_url}:latest
+docker pull mujju1716/amazon-nginx:latest
 
-docker run -d -p 80:80 ${aws_ecr_repository.app.repository_url}:latest
+docker rm -f web || true
+
+docker run -d \
+  --name web \
+  --restart=always \
+  -p 80:80 \
+  mujju1716/amazon-nginx:latest
 EOF
-
-  depends_on = [null_resource.docker_push]
 }
 
+################################
+# Attach EC2 to Target Group
+################################
 resource "aws_lb_target_group_attachment" "attach" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.app.id
